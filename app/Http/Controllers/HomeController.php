8438d306ab1bc6d75f6d08fff8b929450ecdae48
Fernando Luiz de Carvalho Barbosa <fernando.barbosa@TITO-SCS.LOCAL>
2020-12-04 16:28:21 -0300

    Pagina inicial são as postagens do usuario

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -24,7 +24,7 @@ class HomeController extends Controller
     public function index()
     {
     
-        return redirect('/postagens');
+        return redirect('/postagens/usuario');
 
     }
 }
